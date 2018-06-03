d3ae3368e1f3baf0c5d7fd8fff904bfa0a1c88c2
clauber115 <clauber115@gmail.com>
2018-06-03 18:28:31 -0500

    Initial project upload
    This is a base working version of a game skeleton made using the "Make A 3D Platformer" youtube tutorial. This will be used as a starting point for this project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+
+    public Vector3 offset;
+
+    public bool useOffsetValues;
+
+    public float rotateSpeed;
+
+    public bool invertCamera;
+
+    public Transform pivot;
+
+	// Use this for initialization
+	void Start () {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        //hide cursor
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        //Get the x position of the mouse & rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        //Get the Y position of the mouse & rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed * -1;
+        if (invertCamera)
+        {
+            vertical = vertical * -1;
+        }
+        pivot.Rotate(vertical, 0, 0);
+
+        //Move Camera based on the current rotation of the target & the original offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset); //order of multipliction matters. why?
+
+        //transform.position = target.position - offset;
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
+        }
+
+        transform.LookAt(target);		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveVel;
+    //public Rigidbody theRB;
+    public CharacterController controller;
+    public float jumpForce;
+
+    private Vector3 moveDir;
+    public float gravityScale;
+
+	// Use this for initialization
+	void Start () {
+        //theRB = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+	}
+
+    //Update is called once per frame
+    void Update()
+    {
+        //don't put movement here to avoid jerkiness in motion
+    }
+    // LateUpdate is called after every Update()
+    void LateUpdate()
+    {
+        /*theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveVel, theRB.velocity.y, Input.GetAxis("Vertical") * moveVel);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }*/
+
+        //moveDir = new Vector3(Input.GetAxis("Horizontal") * moveVel, moveDir.y, Input.GetAxis("Vertical") * moveVel);
+
+        //Move by camera orientation
+        //but don't fuck up gravity
+        float yStore = moveDir.y;
+        moveDir = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDir = moveDir.normalized * moveVel;
+        moveDir.y = yStore; //this saves gravity from getting normalized
+
+        if (controller.isGrounded)
+        {
+            moveDir.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpForce;
+            }
+        }
+
+        moveDir.y = moveDir.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDir * Time.deltaTime);
+    }
+}
